a752da75390e68b1f1f8a768ca9d72668ef996da
Sarp Vahabi <sarp.vahabi@outlook.com>
2023-07-29 12:21:42 +0300

    3rd

--- IzmirBel.Survey/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ IzmirBel.Survey/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -84,7 +84,7 @@ namespace IzmirBel.Survey.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(128, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 12)] //ASVS 2.1.1
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
